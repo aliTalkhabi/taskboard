56048ca1e254df96c251f57c2ef01d56a4607ae3
aliTalkhabi <atdes1999@gmail.com>
2025-10-17 14:50:27 +0330

    implement change status in cards

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -43,7 +43,7 @@ const Board: React.FC<BoardProps> = ({ type }) => {
   }, [type]);
 
   return type === "todo" ? (
-    <div className="border-2 border-red-400 rounded-2xl bg-white/50 shadow-md relative overflow-hidden p-2">
+    <div className="border-2 border-red-400 rounded-2xl bg-white/50 shadow-md relative overflow-x-hidden p-2">
       <h1 className="w-full h-10 text-center bg-red-200 flex justify-center items-center rounded">
         تسک های تعریف شده
       </h1>
@@ -61,7 +61,7 @@ const Board: React.FC<BoardProps> = ({ type }) => {
       </div>
     </div>
   ) : type === "inwork" ? (
-    <div className="border-2 border-yellow-400 rounded-2xl bg-white/50 shadow-md relative overflow-hidden p-2">
+    <div className="border-2 border-yellow-400 rounded-2xl bg-white/50 shadow-md relative overflow-x-hidden p-2">
       <h1 className="w-full h-10 text-center bg-yellow-200 flex justify-center items-center rounded">
         تسک های درحال انجام
       </h1>{" "}
@@ -78,7 +78,7 @@ const Board: React.FC<BoardProps> = ({ type }) => {
       </div>
     </div>
   ) : type === "done" ? (
-    <div className="border-2 border-green-400 rounded-2xl bg-white/50 shadow-md relative overflow-hidden p-2">
+    <div className="border-2 border-green-400 rounded-2xl bg-white/50 shadow-md relative overflow-x-hidden p-2">
       <h1 className="w-full h-10  text-center bg-green-200 flex justify-center items-center rounded">
         تسک های انجام شده
       </h1>{" "}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,19 +2,46 @@
 import { DeleteIcon, EditIcon } from "@/utils/icons";
 import { Card as CardTypes } from "@/types";
 import Link from "next/link";
+import React, { useState } from "react";
+import { updateCard } from "@/utils/api";
 
 type Props = {
   card: CardTypes;
 };
 
 const Card = ({ card }: Props) => {
-  const { title, description, id } = card;
+  const { title, description, id, status } = card;
+  const [changestatus, setChangeStatus] = useState<CardTypes["status"]>(
+    status ?? undefined
+  );
+
+  async function onStatusChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    const newStatus = e.target.value as CardTypes["status"];
+    setChangeStatus(newStatus);
+    try{
+      await updateCard(id ,{status:newStatus})
+      window.dispatchEvent(new Event("cards-updated"));
+    }catch(err){
+        console.error(err)
+    }
+  }
   return (
     <div className="border-2 border-slate-500 rounded mx-auto my-2 p-2 flex flex-wrap ">
       <div className="w-full">
         <div className="flex justify-between items-center my-2">
           <p className="text-base">{title ?? ""}</p>
-          <span className="text-xs">وضعیت </span>
+          <div>
+            <span className="text-xs">وضعیت: </span>
+            <select
+              value={changestatus ?? ""}
+              onChange={onStatusChange}
+              className="text-sm p-1 border rounded "
+            >
+              <option value="todo">تعریف شده</option>
+              <option value="inwork">درحال انجام</option>
+              <option value="done">انجام شده</option>
+            </select>
+          </div>
         </div>
         <p className="h-10 border-gray-100 bg-gray-200 rounded text-xs p-2">
           {description ?? "فاقد توضیحات"}
